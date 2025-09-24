62eff5198bac954fc8757f39860d2f906bcca52e
Jeyson Rodriguez <119008545+JeysonRo@users.noreply.github.com>
2025-09-23 22:06:21 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (12.99%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -7,15 +7,19 @@ class Solution:
         elif len(nums) == 3:
             return max(nums[0] + nums[2], nums[1])
         
-        memo = [0 for i in range(len(nums))]
-        
+        memo = [0 for i in range(4)]
+
+
         memo[0] = nums[0]
         memo[1] = max(nums[1], memo[0])
         memo[2] = max(nums[2] + nums[0], nums[1])
         i = 3
         
         while i < (len(nums)):
-            memo[i] = nums[i] + max(memo[i-2], memo[i-3])
+            memo[3] = nums[i] + max(memo[1], memo[0])
+
             i += 1
+            for j in range(len(memo) - 1):
+                memo[j] = memo[j+1]
         
-        return max(memo[-1], memo[-2])
+        return max(memo[-1], memo[-3])
