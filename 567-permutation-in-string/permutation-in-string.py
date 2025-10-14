911646fa32a0829ec2f7917e5d7b1822d379f1f5
Jeyson Rodriguez <119008545+JeysonRo@users.noreply.github.com>
2025-10-14 19:39:48 -0400

    Time: 19 ms (65.00%) | Memory: 18 MB (39.65%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,7 +1,7 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         bank = {}
-        chartotal = len(s1)
+        charcount = len(s1)
         for c in s1:
             if c in bank:
                 bank[c] += 1
@@ -9,25 +9,24 @@ class Solution:
                 bank[c] = 1
         
         l = r = 0
-        charcount = 0
         while r < len(s2):
             if s2[r] in bank:
                 if bank[s2[r]] <= 0:
                     bank[s2[l]] += 1
                     l += 1
-                    charcount -= 1
+                    charcount += 1
                 else:
                     bank[s2[r]] -= 1
                     r += 1
-                    charcount += 1
+                    charcount -= 1
             else:
                 while l < r:
                     bank[s2[l]] += 1
                     l += 1
-                    charcount -= 1
+                    charcount += 1
                 r += 1
                 l = r
-            if charcount == chartotal:
+            if charcount == 0:
                 return True
 
         return False
