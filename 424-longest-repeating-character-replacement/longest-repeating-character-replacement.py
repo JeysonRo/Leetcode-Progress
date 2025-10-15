a9c831cc83281d4cf2ea2190f8da2656c35485d9
Jeyson Rodriguez <119008545+JeysonRo@users.noreply.github.com>
2025-10-15 00:03:09 -0400

    Time: 139 ms (43.22%) | Memory: 18 MB (49.26%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,43 +1,15 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
+        count = {}
+        res = 0
+
         l = 0
-        r = 0
-        longest = 0
-        mfreq = 0
-        freq = {0:set(), 1: set()}
-        freq[0].add(s[l])
-        bank = {s[l]: 0}
+        for r in range(len(s)):
+            count[s[r]] = 1 + count.get(s[r], 0)
 
-        def insertchar():
-            if s[r] in bank:
-                freq[bank[s[r]]].remove(s[r])
-                bank[s[r]] += 1
-                if bank[s[r]] in freq:
-                    freq[bank[s[r]]].add(s[r])
-                else:
-                    freq[bank[s[r]]] = set([s[r]])
-            else:
-                bank[s[r]] = 1
-                freq[bank[s[r]]].add(s[r])
-
-        def removechar():
-            freq[bank[s[l]]].remove(s[l])
-            bank[s[l]] -= 1
-            freq[bank[s[l]]].add(s[l])
-
-        while r < len(s):
-            insertchar() # highest freq char
-            while mfreq + 1 in freq and len(freq[mfreq + 1]) > 0:
-                mfreq += 1
-            if len(freq[mfreq]) == 0:
-                mfreq -= 1
-            replacements = 1+r-l - mfreq # highest freq char
-            if replacements <= k:
-                longest = max(1+r-l, longest)
-                r += 1
-            else:
-                removechar()
+            if (r-l+1) - max(count.values()) > k:
+                count[s[l]] -= 1
                 l += 1
-                r += 1
-
-        return longest
+            
+            res = max(res, r - l + 1)
+        return res
