6d3cb1d8a6fc3a3504e47dde9a7f488c26341ad1
Jeyson Rodriguez <119008545+JeysonRo@users.noreply.github.com>
2025-11-20 17:28:35 -0500

    Time: 83 ms (32.68%) | Memory: 18.8 MB (90.50%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
@@ -1,30 +1,16 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        
-        adj = {}
+        # Bellman Ford
 
-        for fro, to, price in flights:
-            if fro in adj:
-                adj[fro].append((to, price))
-            else:
-                adj[fro] = [(to, price)]
-        
-        visited = dict()
-        minh = []
-        heapq.heappush(minh, (0, src, k))
-        cost = 0
+        prices = [float("inf")] * n
+        prices[src] = 0
 
-        while minh and dst not in visited:
-            price, src, stops = heapq.heappop(minh)
-            if src in visited and visited[src] > stops:
-                continue
-            cost = max(price, cost)
-            visited[src] = stops
-            if stops >= 0:
-                if src in adj:
-                    for dest, destprice in adj[src]:
-                        heapq.heappush(minh, (destprice + price, dest, stops - 1))
-        
-        if dst not in visited:
-            return -1
-        return cost
+        for i in range(k + 1):
+            tmpprices = prices.copy()
+            for s, d, p in flights:
+                if prices[s] == float("inf"):
+                    continue
+                if prices[s] + p < tmpprices[d]:
+                    tmpprices[d] = prices[s] + p
+            prices = tmpprices
+        return -1 if prices[dst] == float("inf") else prices[dst]
