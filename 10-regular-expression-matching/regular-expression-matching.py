58e095eb57a412d96b47a3972521c9a37396ebed
Jeyson Rodriguez <119008545+JeysonRo@users.noreply.github.com>
2025-11-22 15:28:03 -0500

    Time: 3 ms (94.02%) | Memory: 18.3 MB (9.18%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.py
+++ 10-regular-expression-matching/regular-expression-matching.py
@@ -1,8 +1,11 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        # Brute Force
+        # Top-down memoization DP
+        cache = {}
 
         def dfs(i, j):
+            if (i, j) in cache:
+                return cache[(i, j)]
             if i >= len(s) and j >= len(p):
                 return True
             if j >= len(p):
@@ -11,12 +14,15 @@ class Solution:
             match = i < len(s) and (s[i] == p[j] or p[j] == ".")
 
             if j+1 < len(p) and p[j + 1] == "*":
-                return (dfs(i, j+2) or             # don't use *
-                       (match and dfs(i+1, j)))    # use *
+                cache[(i, j)] = (dfs(i, j+2) or             # don't use *
+                                (match and dfs(i+1, j)))    # use *
+                return cache[(i, j)]
             
             if match:
-                return dfs(i + 1, j + 1)
-            
+                cache[(i, j)] = dfs(i + 1, j + 1)
+                return cache[(i, j)]
+
+            cache[(i, j)] = False
             return False
         
         return dfs(0, 0)
