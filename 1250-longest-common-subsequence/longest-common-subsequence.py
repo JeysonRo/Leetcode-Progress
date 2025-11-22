b07fb8f3fc0294f2475478b60192452bdc9f743b
Jeyson Rodriguez <119008545+JeysonRo@users.noreply.github.com>
2025-11-22 01:55:14 -0500

    Time: 2294 ms (5.04%) | Memory: 202.6 MB (18.59%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -1,13 +1,21 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        
-        dp = [[0 for j in range(len(text2) + 1)] for i in range(len(text1) + 1)]
+        ROW = len(text1)
+        COL = len(text2)
+        dp = {} # key (i, j) : value lcs of text up to that length
 
-        for i in range(len(text1) - 1, -1, -1):
-            for j in range(len(text2) - 1, -1, -1):
+        for i in range(ROW):
+            for j in range(COL):
+                dp[(i, j)] = 0
+
+        for i in range(ROW):
+            for j in range(COL):
                 if text1[i] == text2[j]:
-                    dp[i][j] = 1 + dp[i+1][j+1]
+                    if (i-1,j) in dp and (i,j-1) in dp:
+                        dp[(i,j)] = 1 + dp[(i-1,j-1)]
+                    else:
+                        dp[(i,j)] = 1
                 else:
-                    dp[i][j] = max(dp[i][j+1], dp[i+1][j])
-        
-        return dp[0][0]
+                    dp[(i, j)] = max(dp[(i, j-1)] if (i,j-1) in dp else 0, dp[(i-1,j)] if (i-1,j) in dp else 0)
+
+        return dp[(ROW-1,COL-1)]
