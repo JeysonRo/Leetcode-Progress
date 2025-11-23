1e88b3f7c82b894df1fb551179f7d6137d5b091b
Jeyson Rodriguez <119008545+JeysonRo@users.noreply.github.com>
2025-11-23 16:12:52 -0500

    Time: 7 ms (50.12%) | Memory: 21.3 MB (89.47%) - LeetSync

--- 155-min-stack/min-stack.py
+++ 155-min-stack/min-stack.py
@@ -1,28 +1,26 @@
-from collections import deque
-
 class MinStack:
 
     def __init__(self):
-        self.stack = deque()
-        self.Min = None
+        self.stack = []
+        self.min = []
 
     def push(self, val: int) -> None:
-        self.stack.append((val, self.Min))
-        if self.Min == None:
-            self.Min = val
-        else:
-            self.Min = min(self.Min, val)
-        
-    def pop(self) -> None:
-        stuff = self.stack.pop()
-        self.Min = stuff[1]
+        self.stack.append(val)
+        if not self.min:
+            self.min.append(val)
+        elif self.min[-1] >= val:
+            self.min.append(val)
 
+    def pop(self) -> None:
+        if self.min[-1] == self.stack[-1]:
+            self.min.pop()
+        return self.stack.pop()
 
     def top(self) -> int:
-        return self.stack[-1][0]
+        return self.stack[-1]
 
     def getMin(self) -> int:
-        return self.Min
+        return self.min[-1]
         
 
 
