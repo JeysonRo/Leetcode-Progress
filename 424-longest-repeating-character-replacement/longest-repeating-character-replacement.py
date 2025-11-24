2ddc9f9f78bfb63caf09812971d2800aae75c32e
Jeyson Rodriguez <119008545+JeysonRo@users.noreply.github.com>
2025-11-23 21:28:49 -0500

    Time: 193 ms (18.80%) | Memory: 38.8 MB (23.02%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,17 +1,40 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        count = {}
-        res = 0
+        def increment_char(char):
+            if char not in chars:
+                chars[char] = 0
+                char_counts_index[0].add(char)
+            char_counts_index[chars[char]].remove(char)
+            chars[char] += 1
+            if len(char_counts_index) <= chars[char]:
+                char_counts_index.append(set())
+            char_counts_index[chars[char]].add(char)
 
+        def decrement_char(char):
+            char_counts_index[chars[char]].remove(char)
+            chars[char] -= 1
+            char_counts_index[chars[char]].add(char)
+        
         l = 0
-        maxf = 0
-        for r in range(len(s)):
-            count[s[r]] = 1 + count.get(s[r], 0)
-            maxf = max(maxf, count[s[r]])
+        r = 0
+        char_counts_index = [set()]
+        chars = {}
+        freq = 0
+        while r < len(s):
+            increment_char(s[r])
 
-            if (r-l+1) - maxf > k:
-                count[s[l]] -= 1
-                l += 1
+            if chars[s[r]] > freq:
+                freq = chars[s[r]]
+
+            while len(char_counts_index[freq]) == 0:
+                freq -= 1
+            if freq + k >= r-l + 1:
+                r += 1
+                continue
             
-            res = max(res, r - l + 1)
-        return res
+            r += 1
+            decrement_char(s[l])
+            l += 1
+        
+        return r-l
+    
