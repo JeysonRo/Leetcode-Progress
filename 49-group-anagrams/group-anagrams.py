65629616bcc912df0a50803e27bbd1697c4d94fc
Jeyson Rodriguez <119008545+JeysonRo@users.noreply.github.com>
2025-11-25 18:14:52 -0500

    Time: 19 ms (37.01%) | Memory: 22.8 MB (11.59%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,21 +1,11 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        banks = []
-        res = []
-
-        for i in strs:
-            worddict = {}
-            for j in i:
-                if j in worddict:
-                    worddict[j] += 1
-                else:
-                    worddict[j] = 1
-            if worddict not in banks:
-                banks.append(worddict)
-                res.append([])
-            for k, bank in enumerate(banks):
-                if bank == worddict:
-                    print("I appended " + str(k))
-                    res[k].append(i)
-
-        return res
+        res = defaultdict(list)
+        
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+            res[tuple(count)].append(s)
+        
+        return list(res.values())
