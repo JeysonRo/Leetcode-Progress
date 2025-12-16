ae9fe64a4f8554f9694378a5250026981113e8c4
Jeyson Rodriguez <119008545+JeysonRo@users.noreply.github.com>
2025-12-16 13:30:43 -0500

    Time: 215 ms (21.08%) | Memory: 20.9 MB (38.28%) - LeetSync

--- 542-01-matrix/01-matrix.py
+++ 542-01-matrix/01-matrix.py
@@ -7,26 +7,25 @@ class Solution:
         directions = [(-1,0), (1,0), (0,-1), (0,1)]
 
         queue = deque()
+        visited = set() # (m,n)
 
         for i in range(M):
             for j in range(N):
                 if mat[i][j] == 0:
                     queue.appendleft((i,j))
+                    visited.add((i,j))
         
-        visited = set() # (m,n)
         distance = 0
 
         while queue:
             for i in range(len(queue)):
                 x,y = queue.pop()
-                if (x,y) in visited:
-                    continue
-                visited.add((x,y))
                 res[x][y] = distance
 
                 for dire in directions:
-                    if x+dire[0] >= 0 and x+dire[0] < M and y+dire[1] >= 0 and y+dire[1] < N:
+                    if x+dire[0] >= 0 and x+dire[0] < M and y+dire[1] >= 0 and y+dire[1] < N and (x+dire[0],y+dire[1]) not in visited:
                         queue.appendleft((x+dire[0],y+dire[1]))
+                        visited.add((x+dire[0],y+dire[1]))
             distance += 1
         
         return res
