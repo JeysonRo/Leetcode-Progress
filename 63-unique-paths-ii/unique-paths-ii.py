2ba6862cd99f139338b57e32f3df9f3b7bb66f27
Jeyson Rodriguez <119008545+JeysonRo@users.noreply.github.com>
2026-01-02 18:15:18 -0500

    Time: 0 ms (100.00%) | Memory: 17.3 MB (94.68%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.py
+++ 63-unique-paths-ii/unique-paths-ii.py
@@ -3,10 +3,10 @@ class Solution:
         ROWS = len(obstacleGrid)
         COLS = len(obstacleGrid[0])
 
-        for row in range(ROWS-2, -1, -1):
-            for col in range(COLS-2, -1, -1):
-                if obstacleGrid[row+1][col] == 1 and obstacleGrid[row][col+1] == 1:
-                    obstacleGrid[row][col] = 1
+        # for row in range(ROWS-2, -1, -1):
+        #     for col in range(COLS-2, -1, -1):
+        #         if obstacleGrid[row+1][col] == 1 and obstacleGrid[row][col+1] == 1:
+        #             obstacleGrid[row][col] = 1
         
         dp = [[0 for j in range(COLS)] for i in range(ROWS)]
         dp[ROWS-1][COLS-1] = 1 if obstacleGrid[ROWS-1][COLS-1] == 0 else 0
