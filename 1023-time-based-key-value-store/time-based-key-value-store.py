2e41bd881fc88cb29d44d7795cc86497c1f67f96
Jeyson Rodriguez <119008545+JeysonRo@users.noreply.github.com>
2026-01-22 12:28:08 -0500

    Time: 216 ms (5.25%) | Memory: 69.2 MB (83.99%) - LeetSync

--- 1023-time-based-key-value-store/time-based-key-value-store.py
+++ 1023-time-based-key-value-store/time-based-key-value-store.py
@@ -1,31 +1,29 @@
 class TimeMap:
 
     def __init__(self):
-        self.table = {}
+        self.store = defaultdict(list)
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        if key in self.table:
-            self.table[key].append((value, timestamp))
-        else:
-            self.table[key] = [(value, timestamp)]
+        self.store[key].append((timestamp, value))
 
-    def get(self, key: str, timestamp: int) -> str:
-        if key not in self.table:
+    def get(self, key: str, timestamp: int) -> str: 
+        if len(self.store[key]) == 0:
             return ""
+        # binary search
+        res = ""
         l = 0
-        r = len(self.table[key]) - 1
-        lastvalid = ""
+        r = len(self.store[key]) - 1
+        
         while l <= r:
-            i = (r - l) // 2 + l
-            settime = self.table[key][i][1]
-            if settime <= timestamp:
-                lastvalid = self.table[key][i][0]
-                l = i + 1
-            else:
+            i = l + (r - l) // 2
+            if self.store[key][i][0] > timestamp:
                 r = i - 1
-
-        return lastvalid
-
+            else:
+                res = self.store[key][i][1]
+                l = i + 1
+        
+        return res
+                
 
 # Your TimeMap object will be instantiated and called as such:
 # obj = TimeMap()
