0b25b90a2d467b37d56e8d556074d1d493a99f00
Jeyson Rodriguez <119008545+JeysonRo@users.noreply.github.com>
2026-01-28 14:59:22 -0500

    Time: 125 ms (22.81%) | Memory: 32.6 MB (25.00%) - LeetSync

--- 2145-grid-game/grid-game.py
+++ 2145-grid-game/grid-game.py
@@ -1,16 +1,19 @@
 class Solution:
     def gridGame(self, grid: List[List[int]]) -> int:
         N = len(grid[0])
-        preRow1, preRow2 = grid[0].copy(), grid[1].copy()
+
+        prefix_row1 = [grid[0][i] for i in range(N)]
+        prefix_row2 = [grid[1][i] for i in range(N)]
 
         for i in range(1, N):
-            preRow1[i] += preRow1[i - 1]
-            preRow2[i] += preRow2[i - 1]
+            prefix_row1[i] += prefix_row1[i - 1]
+            prefix_row2[i] += prefix_row2[i - 1]
         
         res = float("inf")
+        
         for i in range(N):
-            top = preRow1[-1] - preRow1[i]
-            bottom = preRow2[i - 1] if i > 0 else 0
+            top = prefix_row1[-1] - prefix_row1[i]
+            bottom = prefix_row2[i - 1] if i > 0 else 0
             secondRobot = max(top, bottom)
             res = min(res, secondRobot)
         return res
