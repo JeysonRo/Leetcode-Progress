92b2c5e9d931c7eb1b4c509555e8d82713660fc4
Jeyson Rodriguez <119008545+JeysonRo@users.noreply.github.com>
2026-01-28 15:36:54 -0500

    Time: 451 ms (27.48%) | Memory: 45.2 MB (5.34%) - LeetSync

--- 1286-constrained-subsequence-sum/constrained-subsequence-sum.py
+++ 1286-constrained-subsequence-sum/constrained-subsequence-sum.py
@@ -1,13 +1,14 @@
 class Solution:
     def constrainedSubsetSum(self, nums: List[int], k: int) -> int:
         res = nums[0]
-        max_heap = [(-nums[0], 0)]
+        max_heap = []
+        heapq.heappush(max_heap, (-nums[0], 0))
 
         for i in range(1, len(nums)):
             while i - max_heap[0][1] > k:
                 heapq.heappop(max_heap)
 
-            cur_max = max(nums[i], nums[i] - max_heap[0][0])
-            res = max(res, cur_max)
-            heapq.heappush(max_heap, (-cur_max, i))
+            cur_dp = max(nums[i], nums[i] - max_heap[0][0])
+            res = max(res, cur_dp)
+            heapq.heappush(max_heap, (-cur_dp, i))
         return res
