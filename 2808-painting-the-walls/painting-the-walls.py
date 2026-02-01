67b1cd9b4c72ce7d0598bdf18a5daa2fe7bc2b85
Jeyson Rodriguez <119008545+JeysonRo@users.noreply.github.com>
2026-02-01 15:06:16 -0500

    Time: 1816 ms (52.35%) | Memory: 470.9 MB (21.48%) - LeetSync

---
+++ 2808-painting-the-walls/painting-the-walls.py
@@ -0,0 +1,15 @@
+class Solution:
+    def paintWalls(self, cost: List[int], time: List[int]) -> int:
+        
+        @lru_cache(None)
+        def dfs(i, remain):
+            if remain <= 0:
+                return 0
+            if i == len(cost):
+                return sys.maxsize
+            
+            paint = cost[i] + dfs(i + 1, remain - 1 - time[i])
+            skip = dfs(i + 1, remain)
+            return min(paint, skip)
+
+        return dfs(0, len(cost))
