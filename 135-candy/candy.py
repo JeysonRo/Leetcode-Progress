315c3324917401b57f5cbfb29581fdcc4f25dabc
Jeyson Rodriguez <119008545+JeysonRo@users.noreply.github.com>
2026-02-03 21:56:57 -0500

    Time: 19 ms (41.62%) | Memory: 21.9 MB (5.56%) - LeetSync

--- 135-candy/candy.py
+++ 135-candy/candy.py
@@ -1,28 +1,17 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
         N = len(ratings)
-        res = [0 for i in range(N)]
+        res = [1 for i in range(N)]
 
-        def recurs(i):
-            if res[i] != 0:
-                return res[i]
-            if i > 0 and i < N - 1:
-                if ratings[i] > ratings[i+1] and ratings[i] > ratings[i-1]:
-                    res[i] = max(recurs(i-1), recurs(i+1)) + 1
-                    return res[i]
-            if i < N - 1:
-                if ratings[i] > ratings[i+1]:
-                    res[i] = recurs(i+1) + 1
-                    return res[i]
-            if i > 0:
-                if ratings[i] > ratings[i-1]:
-                    res[i] = recurs(i-1) + 1
-                    return res[i]
-            res[i] = 1
-            return res[i]
-
-        final = 0
-        for i in range(N):
-            final += recurs(i)
+        for i in range(1, N):
+            if ratings[i] > ratings[i-1]:
+                res[i] = res[i-1] + 1
         
+        for i in range(N-2, -1, -1):
+            if ratings[i] > ratings[i+1]:
+                res[i] = max(res[i], res[i+1] + 1)
+        
+        final = 0
+        for num in res:
+            final += num
         return final
