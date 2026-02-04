e430f736beee128829e4a768cf6087a22cf12075
Jeyson Rodriguez <119008545+JeysonRo@users.noreply.github.com>
2026-02-03 23:34:06 -0500

    Time: 11 ms (85.98%) | Memory: 22.7 MB (9.15%) - LeetSync

---
+++ 956-number-of-music-playlists/number-of-music-playlists.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numMusicPlaylists(self, n: int, goal: int, k: int) -> int:
+        mod = 10**9 + 7
+
+        @cache
+        def count(cur_goal, old_songs):
+            if cur_goal == 0 and old_songs == n:
+                return 1
+            if cur_goal == 0 or old_songs > n:
+                return 0
+
+            # choose a new song
+            res = (n - old_songs) * count(cur_goal-1, old_songs + 1)
+
+            # choose an old song
+            if old_songs > k:
+                res += (old_songs - k) * count(cur_goal-1, old_songs)
+            
+            return res % mod
+        
+        return count(goal, 0)
