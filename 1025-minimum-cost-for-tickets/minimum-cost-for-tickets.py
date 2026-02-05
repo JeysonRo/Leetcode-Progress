3bed48820b95d4463bbff577a93a560a8c5476c9
Jeyson Rodriguez <119008545+JeysonRo@users.noreply.github.com>
2026-02-05 16:41:21 -0500

    Time: 11 ms (16.14%) | Memory: 19.7 MB (14.54%) - LeetSync

---
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.py
@@ -0,0 +1,25 @@
+class Solution:
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+        N = len(days)
+        dp = [0 for i in range(N)]
+
+        if costs[2] < costs[1]:
+            costs[1] = costs[2]
+        if costs[1] < costs[0]:
+            costs[0] = costs[1]
+
+        def dfs(i):
+            if i == N:
+                return 0
+            if dp[i] != 0:
+                return dp[i]
+
+            dp[i] = N * costs[2]
+            for d, c in zip([1, 7, 30], costs):
+                j = i
+                while j < len(days) and days[j] < days[i] + d:
+                    j += 1
+                dp[i] = min(dp[i], c + dfs(j))
+            return dp[i]
+        
+        return dfs(0)
