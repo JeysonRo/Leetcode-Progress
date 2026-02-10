6b516dc38d0d366610d6607664aaac52d43e36b4
Jeyson Rodriguez <119008545+JeysonRo@users.noreply.github.com>
2026-02-10 17:04:36 -0500

    Time: 16 ms (86.64%) | Memory: 31.1 MB (12.79%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.py
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.py
@@ -3,30 +3,20 @@ class Solution:
         # cardPoints = list of cards and their point values
         # each step, take 1 card from front or back
         # allowed "k" steps
-
-        left_prefix = [0 for i in cardPoints]
-        right_prefix = [0 for i in cardPoints]
-
-        left_prefix[0] = cardPoints[0]
-        for i in range(1, len(left_prefix)):
-            left_prefix[i] = left_prefix[i-1] + cardPoints[i]
-
-        right_prefix[len(right_prefix)-1] = cardPoints[len(right_prefix)-1]
-        for i in range(len(right_prefix)-2, -1, -1):
-            right_prefix[i] = right_prefix[i+1] + cardPoints[i]
         
-        if k >= len(cardPoints):
-            return right_prefix[0]
+        prefix = 0
+        for i in range(k):
+            prefix += cardPoints[i]
         
-        l = k - 1
-        r = len(cardPoints)
-        res = left_prefix[l]
-        while l >= -1:
-            leftsum = 0 if l < 0 else left_prefix[l]
-            rightsum = 0 if r >= len(right_prefix) else right_prefix[r]
-            windowsum = leftsum + rightsum
-            res = max(res, windowsum)
+        l = k
+        r = len(cardPoints) - 1
+        res = prefix
+
+        while l >= 0:
+            res = max(res, prefix)
             l -= 1
+            prefix -= cardPoints[l]
+            prefix += cardPoints[r]
             r -= 1
         
         return res
