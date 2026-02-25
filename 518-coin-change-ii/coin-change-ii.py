89cada2ddfa0be5b46f82281606e59830b4fc9a4
Jeyson Rodriguez <119008545+JeysonRo@users.noreply.github.com>
2026-02-24 22:51:19 -0500

    Time: 1668 ms (5.56%) | Memory: 519 MB (5.05%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -1,19 +1,20 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        
-        dp = [[0] * (len(coins) + 1) for i in range(amount + 1)]
-        dp[0] = [1] * (len(coins) + 1)
-        # dp[amount][denominations]
 
-        for a in range(1, amount + 1):
-            for d in range(len(coins)):
-                if coins[d] > a:
-                    if d == 0:
-                        continue
-                    dp[a][d] = dp[a][d-1]
-                else:
-                    if d == 0:
-                        dp[a][d] = dp[a - coins[d]][d]
-                    else:
-                        dp[a][d] = dp[a][d-1] + dp[a - coins[d]][d]
-        return dp[amount][len(coins)-1]
+        @cache
+        def backtrack(amount, i):
+            if i >= len(coins):
+                return 0
+            if amount == 0:
+                return 1
+            if amount < 0:
+                return 0
+            res = 0
+            # use this denomination
+            denomination = coins[i]
+            res += backtrack(amount-denomination, i)
+            # skip this denomination
+            res += backtrack(amount, i + 1)
+            return res
+        
+        return backtrack(amount, 0)
