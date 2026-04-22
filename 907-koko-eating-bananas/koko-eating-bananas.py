36c64ce2aecc8336c20cf8b3a16dd7defc166316
Jeyson Rodriguez <119008545+JeysonRo@users.noreply.github.com>
2026-04-22 13:11:27 -0400

    Time: 174 ms (42.95%) | Memory: 20.7 MB (48.06%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,19 +1,28 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        # get minimum eating speed that finishes under h time
+        # range between 1 and max(piles)
         # binary search
-
+        # find k such that cur_time > h and k+1 where cur_time <= h
         r = max(piles)
-        lowest_k = r
         l = 1
-        while l <= r:
-            k = (r-l) // 2 + l
-            hours_spent = 0
-            for e in piles:
-                hours_spent += math.ceil(e / k)
-            if hours_spent > h:
-                l = k+1
-            else:
-                r = k-1
-                lowest_k = min(lowest_k, k)
+        while l < r:
+            k = l + (r - l) // 2 
+            cur_h = 0
+            for pile in piles:
+                if k >= pile:
+                    cur_h += 1
+                else:
+                    cur_h += math.ceil(pile / k)
+                    
+            if cur_h > h: # K IS TOO SLOW
+                l = k + 1
+            else: # K is in time
+                r = k
         
-        return lowest_k
+        return r
+        # [3,6,7,11] 8
+        # l = 1
+        # r = 11
+        # k = 5
+        # cur = 5
