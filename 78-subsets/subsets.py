8a4fc1ca2a72d2eacaaa769f478294816e9c9f51
Jeyson Rodriguez <119008545+JeysonRo@users.noreply.github.com>
2026-04-24 14:59:50 -0400

    Time: 0 ms (100.00%) | Memory: 19.4 MB (32.38%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,8 +1,15 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = [[]]
-        for i in nums:
-            for j in range(len(res)):
-                res.append(res[j] + [i])
-        
+        res = []
+        def recurs(cur_set, i):
+            # decide, keep or skip element
+            if i >= len(nums):
+                res.append(cur_set)
+                return
+            recurs(cur_set, i+1)
+            recurs(cur_set + [nums[i]], i+1)
+            return
+
+        recurs([], 0)
+
         return res
