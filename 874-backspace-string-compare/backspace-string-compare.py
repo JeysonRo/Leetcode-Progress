3b121b7c1dda74a05cd96abd9143909c391f0efe
Jeyson Rodriguez <119008545+JeysonRo@users.noreply.github.com>
2026-04-25 18:40:46 -0400

    Time: 0 ms (100.00%) | Memory: 19.2 MB (67.49%) - LeetSync

---
+++ 874-backspace-string-compare/backspace-string-compare.py
@@ -0,0 +1,22 @@
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        sa = []
+        ta = []
+
+        for i in range(len(s)):
+            if s[i] == '#':
+                if sa:
+                    sa.pop()
+            else:
+                sa.append(s[i])
+        
+        for i in range(len(t)):
+            if t[i] == '#':
+                if ta:
+                    ta.pop()
+            else:
+                ta.append(t[i])
+
+        if ta == sa:
+            return True
+        return False
