5e3f38e5320934db12831552a79df29c3c3b935b
Jeyson Rodriguez <119008545+JeysonRo@users.noreply.github.com>
2026-05-05 13:58:26 -0400

    Time: 43 ms (11.34%) | Memory: 27.6 MB (21.15%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,28 +1,29 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        product = 1
-        answer = []
-        zeros = 0
-
-        for i in nums:
-            if i == 0:
-                zeros += 1
-            else:
-                product = product * i
-
+        
+        left_product = []
+        cur_product = 1
         for i in range(len(nums)):
-            if zeros >= 2:
-                answer.append(0)
-            elif zeros == 1:
-                if nums[i] == 0:
-                    answer.append(int(product))
-                else:
-                    answer.append(0)
+            cur_product *= nums[i]
+            left_product.append(cur_product)
+
+        right_product = [1 for i in nums]
+        cur_product = 1
+        for i in range(len(nums)-1,-1,-1):
+            cur_product *= nums[i]
+            right_product[i] = cur_product
+        
+        res = []
+        for i in range(len(nums)):
+            if i-1 >= 0:
+                left = left_product[i-1]
             else:
-                if nums[i] == 0:
-                    answer.append(int(product))
-                else:
-                    answer.append(int(product / nums[i]))
-
-
-        return answer
+                left = 1
+            if i+1 < len(nums):
+                right = right_product[i+1]
+            else:
+                right = 1
+            product = left * right
+            res.append(product)
+        
+        return res
