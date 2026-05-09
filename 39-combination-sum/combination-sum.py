93c89a0e6cfb6d52a1d3d2f200fe4be56800121a
Jeyson Rodriguez <119008545+JeysonRo@users.noreply.github.com>
2026-05-08 22:01:30 -0400

    Time: 15 ms (21.91%) | Memory: 19.6 MB (64.60%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,18 +1,25 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        # decision tree
+
+        # add current number to sum
+        # increment i to next number
         res = []
-        path = []
-        def combination(i, target):
+        combination = []
+        def recurs(i, cur_sum):
             if i >= len(candidates):
                 return
-            if target > 0:
-                path.append(candidates[i])
-                combination(i, target - candidates[i])
-                path.pop()
-                combination(i+1, target)
-            elif target == 0:
-                res.append(path.copy())
-            return
+            if cur_sum == target:
+                res.append(combination.copy())
+                return
+            if cur_sum > target:
+                return
+            
+            combination.append(candidates[i])
+            recurs(i, cur_sum + candidates[i])
+            combination.pop()
+
+            recurs(i+1, cur_sum)
         
-        combination(0, target)
+        recurs(0, 0)
         return res
