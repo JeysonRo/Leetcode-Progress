cd79a6fec02c120c53f33737217b06c3cad58fb3
Jeyson Rodriguez <119008545+JeysonRo@users.noreply.github.com>
2026-05-09 19:32:27 -0400

    Time: 283 ms (52.73%) | Memory: 19.4 MB (37.16%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,41 +1,32 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        def isPali(string: str):
-            l = 0
-            r = len(string) - 1
-            while l < r:
-                if string[l] != string[r]:
-                    return False
-                l += 1
-                r -= 1
-            return True
-        
-        if len(s) == 1:
-            return s
-        if len(s) == 2:
-            if s[0] == s[1]:
-                return s
-            else:
-                return s[0]
 
-        l = 0
-        r = 1
-        longest = s[0]
-        swap = True
-
-        while r < len(s):
-            if isPali(s[l:r + 1]):
-                longest = s[l:r+1]
-                if l == 0:
+        largest = 0
+        largest_string = ''
+        for i in range(len(s)):
+            # odd
+            l = i
+            r = i
+            while l >= 0 and r < len(s):
+                if s[l] == s[r]:
+                    if r-l+1 > largest:
+                        largest = r-l + 1
+                        largest_string = s[l:r+1]
+                    l -= 1
                     r += 1
                 else:
+                    break
+            # even
+            l = i
+            r = i + 1
+            while l >= 0 and r < len(s):
+                if s[l] == s[r]:
+                    if r-l+1 > largest:
+                        largest = r-l + 1
+                        largest_string = s[l:r+1]
                     l -= 1
-                swap = True
-                continue
-            if swap:
-                r += 1
-            else:
-                l += 1
-            swap = not swap
-
-        return longest
+                    r += 1
+                else:
+                    break
+        
+        return largest_string
