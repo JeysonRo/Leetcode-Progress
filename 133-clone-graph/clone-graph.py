8203ba88d522ebe1052e9c559f70c85f2c520141
Jeyson Rodriguez <119008545+JeysonRo@users.noreply.github.com>
2026-05-10 18:32:25 -0400

    Time: 51 ms (49.55%) | Memory: 19.8 MB (18.49%) - LeetSync

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -1,28 +1,35 @@
-# """
-# # Definition for a Node.
-# class Node:
-#     def __init__(self, val = 0, neighbors = None):
-#         self.val = val
-#         self.neighbors = neighbors if neighbors is not None else []
-# """
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
 
 from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        if node is None:
-            return None
+        # bfs
 
-        oldToNew = {}
-
+        table = {} # original node : copy node
+        
         def dfs(node):
-            if node in oldToNew:
-                return oldToNew[node]
-
-            copy = Node(node.val)
-            oldToNew[node] = copy
-
-            for i in node.neighbors:
-                copy.neighbors.append(dfs(i))
-            return copy
+            if not node:
+                return None
+            
+            if node not in table:
+                # create copy, add to table
+                node_copy = Node(node.val)
+                table[node] = node_copy
+            
+                if node.neighbors:
+                    node_copy.neighbors = []
+                    for neighbor in node.neighbors:
+                        node_copy.neighbors.append(dfs(neighbor))
+            
+            else:
+                node_copy = table[node]
+            
+            return node_copy
         
         return dfs(node)
