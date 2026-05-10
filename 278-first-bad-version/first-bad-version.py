73ab52f072c003a342d23fa172a93ad85438cf2a
Jeyson Rodriguez <119008545+JeysonRo@users.noreply.github.com>
2026-05-09 20:04:07 -0400

    Time: 47 ms (39.66%) | Memory: 19.2 MB (65.01%) - LeetSync

--- 278-first-bad-version/first-bad-version.py
+++ 278-first-bad-version/first-bad-version.py
@@ -3,17 +3,16 @@
 
 class Solution:
     def firstBadVersion(self, n: int) -> int:
-        # binary search
-
+        
         l = 1
         r = n
 
         while l <= r:
-            i = (l + r) // 2
-            bad = isBadVersion(i)
-            if bad:
+            i = l + (r - l) // 2
+            isbad = isBadVersion(i)
+            if isbad:
                 r = i - 1
             else:
                 l = i + 1
-
-        return r + 1            
+        
+        return r + 1
