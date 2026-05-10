0f9d5fca0ec4c59522ea0a287fccfc7499d665db
Jeyson Rodriguez <119008545+JeysonRo@users.noreply.github.com>
2026-05-10 18:06:56 -0400

    Time: 151 ms (59.74%) | Memory: 37 MB (46.00%) - LeetSync

---
+++ 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.py
@@ -0,0 +1,33 @@
+class Solution:
+    def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
+        if len(profit) == 0:
+            return 0
+        
+        queue = [] # (time, profit)
+        dp = {} # (time, profit)
+
+        jobs = [[startTime[i], endTime[i], profit[i]] for i in range(len(profit))]
+
+        jobs.sort(key=lambda x : (x[0], x[1], x[2]))
+
+        i = 0
+        cur_profit = 0
+        while i < len(jobs):
+            cur_time = jobs[i][0]
+            while queue and cur_time >= queue[0][0]:
+                job_end, job_profit = heapq.heappop(queue)
+                cur_profit = max(cur_profit, job_profit)
+
+            heapq.heappush(queue, (jobs[i][1], cur_profit + jobs[i][2]))
+            i += 1
+        
+        while queue:
+            job_end, job_profit = heapq.heappop(queue)
+            cur_profit = max(cur_profit, job_profit)
+        
+        return cur_profit
+
+        # cur_profit = 20
+        # time = 3
+        # 5, 20
+        # 10, 120
